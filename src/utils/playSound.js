5d3e0c706c7e26fe52d6827090af060e4f9f2834
Jiahou Liu <twtialn@gmail.com>
2024-02-11 14:09:29 -0800

    Refactor: improve utils efficiency

--- src/utils/playSound.js
+++ src/utils/playSound.js
@@ -1,20 +1,26 @@
+// Define audio objects at the top level to ensure they are created only once.
+const sounds = {
+  correct: new Audio('/sounds/correct.mp3'),
+  wrong: new Audio('/sounds/wrong.mp3'),
+  win: new Audio('/sounds/win.mp3'),
+  start: new Audio('/sounds/countdown.mp3'),
+};
+
+// Play functions just need to reference these objects and play them.
 function playCorrectSound() {
-  setSoundObject('/sounds/correct.mp3');
+  sounds.correct.play();
 }
 
 function playWrongSound() {
-  setSoundObject('/sounds/wrong.mp3');
+  sounds.wrong.play();
 }
 
 function playWinSound() {
-  setSoundObject('/sounds/win.mp3');
+  sounds.win.play();
 }
 
 function playStartSound() {
-  setSoundObject('/sounds/countdown.mp3');
+  sounds.start.play();
 }
-function setSoundObject(soundPath) {
-  const sound = new Audio(soundPath);
-  sound.play();
-}
+
 export { playCorrectSound, playWrongSound, playWinSound, playStartSound };
